6c9366049a91371933a5dd72445ca0f255e19845
Feliciasharon <75035335+Feliciasharon@users.noreply.github.com>
2026-02-06 11:28:33 -0500

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_URL = "https://resume-rag-backend.onrender.com";
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://rag-chat-llm.onrender.com";
+//const API_URL = "http://127.0.0.1:8000";
 
 export const askQuestion = async (query: string) => {
   const response = await axios.post(`${API_URL}/ask`, { query });
